1a4afbdac132f6af62d24deb0be1deb090273a37
DMenace92 <dennisenwiya@gmail.com>
2024-05-09 18:42:10 -0700

    added changes to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
-// const express = require("express");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 const PORT = process.env.PORT || 9000;
